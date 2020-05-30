a92f5e1a2a588a76571530676d7c59b4394a0f8d
cnj <nakju04@gmail.com>
2020-05-30 22:11:38 +0900

    AlarmViewModel
    ViewModel

---
+++ app/src/main/java/com/example/aralarm/AlarmViewModel.java
@@ -0,0 +1,25 @@
+package com.example.aralarm;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class AlarmViewModel extends AndroidViewModel {
+
+    private AlarmRepository mRepository;
+
+    private LiveData<List<Alarm>> mAllAlarms;
+
+    public AlarmViewModel (Application application){
+        super(application);
+        mRepository = new AlarmRepository(application);
+        mAllAlarms = mRepository.getAllAlarm();
+    }
+
+    LiveData<List<Alarm>> getAllAlarms() {return mAllAlarms;}
+
+    public void insert(Alarm alarm){mRepository.insert(alarm);}
+}
