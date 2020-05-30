e6ca37d12740ca33f0d97ca7d6ad4655f7b3124b
cnj <nakju04@gmail.com>
2020-05-30 22:02:11 +0900

    AlarmRoomDatabase
    instance

---
+++ app/src/main/java/com/example/aralarm/AlarmRoomDatabase.java
@@ -0,0 +1,36 @@
+package com.example.aralarm;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {Alarm.class}, version = 1, exportSchema = false)
+public abstract class AlarmRoomDatabase extends RoomDatabase {
+
+    public abstract AlarmDao alarmDao();
+
+    private static volatile AlarmRoomDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static AlarmRoomDatabase getDatabase(final Context context){
+        if(INSTANCE == null){
+            synchronized (AlarmRoomDatabase.class){
+                if(INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AlarmRoomDatabase.class, "alarm_database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    // call back 안씀.
+
+}
