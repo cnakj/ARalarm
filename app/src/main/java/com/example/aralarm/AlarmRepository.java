e71af5a650c608238ccc1c21ac5193752b65c2b7
cnj <nakju04@gmail.com>
2020-05-30 22:07:58 +0900

    AlarmRepository
    repository

---
+++ app/src/main/java/com/example/aralarm/AlarmRepository.java
@@ -0,0 +1,27 @@
+package com.example.aralarm;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+class AlarmRepository {
+
+    private AlarmDao mAlarmDao;
+    private LiveData<List<Alarm>> mAllAlarm;
+
+    AlarmRepository(Application application){
+        AlarmRoomDatabase db = AlarmRoomDatabase.getDatabase(application);
+        mAlarmDao = db.alarmDao();
+        mAllAlarm = mAlarmDao.getOrderedWords();
+    }
+
+    LiveData<List<Alarm>> getAllAlarm() {return mAllAlarm;}
+
+    void insert(Alarm alarm){
+        AlarmRoomDatabase.databaseWriteExecutor.execute(() ->{
+            mAlarmDao.insert(alarm);
+        });
+    }
+}
