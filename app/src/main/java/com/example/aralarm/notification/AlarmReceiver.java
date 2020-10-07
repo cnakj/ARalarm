d436aaa1ab5ab70c0a9b4f008e02a3d89d53d375
cnj <nakju04@gmail.com>
2020-10-07 23:43:40 +0900

    알람 울리기 noti에 안넣고 ringtone으로 변경

--- app/src/main/java/com/example/aralarm/notification/AlarmReceiver.java
+++ app/src/main/java/com/example/aralarm/notification/AlarmReceiver.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 
@@ -19,10 +20,15 @@ public class AlarmReceiver extends BroadcastReceiver {
     private String CHANNEL_ID = "aralarm";
     private String CHANNEL_NAME = "aralarm";
     private String CHANNEL_DESCRIPTION = "alalarm";
+    private int NOTIFICATION_ID = 1234;
 
     @Override
     public void onReceive(Context context, Intent intent) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+        Ringtone ringtone = RingtoneManager.getRingtone(context, notification);
+        ringtone.play();
 
         Intent notificationIntent = new Intent(context, GameActivity.class);
         PendingIntent notificationPendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
@@ -38,24 +44,16 @@ public class AlarmReceiver extends BroadcastReceiver {
             }
         }else builder.setSmallIcon(R.mipmap.ic_launcher); // mipmap
 
-        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        if(alarmSound == null){
-            alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-            if(alarmSound == null){
-                alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            }
-        }
 
         builder.setDefaults(NotificationCompat.DEFAULT_ALL)
                 .setWhen(System.currentTimeMillis())
-                .setSound(alarmSound)
                 .setContentTitle("알람")
+                .setFullScreenIntent(notificationPendingIntent, true)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_ALARM)
-                .setFullScreenIntent(notificationPendingIntent, true);
+                .setCategory(NotificationCompat.CATEGORY_ALARM);
 
         if(notificationManager != null)
-            notificationManager.notify(1234, builder.build());
+            notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
 
