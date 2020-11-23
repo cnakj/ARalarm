8c5099a0ab8465fcc0f0d4508fa67952062b0ab8
nakju <nakju04@gmail.com>
2020-11-24 06:06:55 +0900

    게임 연결까지 됐음

--- app/src/main/java/com/example/aralarm/activity/GameActivity.java
+++ app/src/main/java/com/example/aralarm/activity/GameActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.aralarm.ApplicationClass;
 import com.example.aralarm.R;
+import com.unity3d.player.UnityPlayerActivity;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -22,13 +25,24 @@ public class GameActivity extends AppCompatActivity {
         applicationClass = (ApplicationClass)getApplicationContext();
         NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
 
+        Button btn_game = findViewById(R.id.btn_game);
+        btn_game.setOnClickListener(v -> {
+            Intent intent = new Intent(GameActivity.this, UnityPlayerActivity.class);
+            startActivityForResult(intent, 1215);
+        });
+
         Button btn_off = findViewById(R.id.btn_off);
         btn_off.setOnClickListener(v -> {
             notificationManager.cancel(1234);
             applicationClass.ringtone.stop();
             finish();
         });
+    }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Button btn2 = findViewById(R.id.btn_off);
+        btn2.setVisibility(View.VISIBLE);
     }
 }
